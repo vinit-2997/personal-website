cb3bbb6f7a408e398afbbda9786acdf73c883611
vinit2997 <91102135+vinit2997@users.noreply.github.com>
2022-02-08 17:27:50 -0600

    updated resume

--- code/src/components/About/AboutCard.js
+++ code/src/components/About/AboutCard.js
@@ -8,37 +8,60 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Vinit Mahendra Todai,  </span>
-            a creative software engineer with over 2 years of experience in software development and artificial intelligence.<br/> <br/> I'm currently pursuing a Master's degree in Artificial Intelligence at Northwestern University, which is ranked among the top ten universities in the United States.
-            Prior to this, I worked as a Software Developer at Barclays - the bank who invented the first ATM machine...!.
+            Hi Everyone, I am <span className="purple">Vinit Todai,  </span>
+            I am Vinit, a creative software engineer with over 2 years of experience in the corporate world.<br/> <br/> 
+            I'm currently pursuing a Master's degree in Artificial Intelligence at Northwestern University, which is ranked among the top ten universities in the United States. Prior to this, I worked as a Software Developer at Barclays - the bank who invented the first ATM machine...!
             <br />
             <br />
-            I have been experimenting a lot in the field of Artificial Intelligence and and its applications in Robotics, Virtual Reality, Augmented Reality, Internet of Things, and Brain computer interfaces.
+            At Barclays, I had an incredible opportunity to work on the development of cutting-edge software solutions for Barclays Corporate Banking, which impacted the lives of millions of people worldwide.            <br />
             <br />
+            I love to explore different technologies and have been experimenting a lot in the fields of Artificial Intelligence, Robotics, Virtual Reality, Augmented Reality, Internet of Things, and Brain computer interfaces.            <br/>
             <br />
-            I'm looking forward to working for an innovative firm where I can leverage my Artificial intelligence and machine learning expertise, as well as my prior experience as a software developer at Barclays. I want to work on challenging problems, create incredible products, and work for a company that is out to put a dent in the universe.
-            <br/>
+            I'm looking forward to working for an innovative firm where I can leverage my expertise, as well as my prior experience as a software developer at Barclays. I want to work on challenging problems, create incredible products, and put a dent in the universe!
             Do checkout some of my projects here!
           </p>
-          {/* <ul>
+          <br />
+          <p style={{ textAlign: "justify" }}>
+
+          <div style={{ borderTop: "2px solid #fff ", marginBottom: 20 }}></div>
+          <h3><span className="white">Technical Skills:   </span></h3>
+
+               <h6>-  <span className="purple">Languages </span> : Java, Python, C++, MySQL </h6> 
+               <h6>- <span className="purple">Web Technologies </span> : Microservices, Spring, Hibernate, RESTful API, JSON, HTML, CSS, Javascript, Angular, Node.js </h6> 
+               <h6>- <span className="purple">Frameworks/Tools </span> : Git, Eclipse, IntelliJ, Jenkins, Openshift, SonarQube, Jira, Agile Central </h6> 
+          </p>
+
+          {/* { <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Web Technologies: Microservices, Spring, Hibernate, RESTful API, JSON, HTML, CSS, Javascript, Angular, Node.js
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Frameworks/Tools: Git, Eclipse, IntelliJ, Jenkins, Openshift, SonarQube, Jira, Agile Central
             </li>
-          </ul> */}
+          </ul> } */}
+
 
           {/* <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Soumyajit</footer> */}
         </blockquote>
+
+        
       </Card.Body>
     </Card>
+
+
+
+
+
+
+
+
+
+    
   );
 }
 
--- code/src/components/Projects/Projects.js
+++ code/src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ import ASL from "../../Assets/Projects/ASL1.png";
 
 
 
-import '/Users/vinit2/Documents/GitHub/vinit-2997.github.io/code/src/components/Projects/style_proj.css';
+import '/Users/vinit2997/Documents/GitHub/vinit-2997.github.io/code/src/components/Projects/style_proj.css';
 
 function Projects() {
   return (
